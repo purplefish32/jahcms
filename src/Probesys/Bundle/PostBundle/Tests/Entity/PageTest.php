ffdb6be8113047cd060a24ab2d30dec96789709e
Donovan Tengblad <purplefish32@gmail.com>
2012-10-15 08:18:53 +0200

    Added simple unit test for pages

---
+++ src/Probesys/Bundle/PostBundle/Tests/Entity/PageTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Probesys\Bundle\PostBundle\Tests\Entity;
+
+use Probesys\Bundle\PostBundle\Entity\Post;
+
+class PageTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $post = new Post();
+        $post->setPostTitle("My Page");
+        $post->setPostType("page");
+
+        $this->assertEquals($post->getPostTitle(), "My Page");
+        $this->assertEquals($post->getPostType(), "page");
+    }
+}
